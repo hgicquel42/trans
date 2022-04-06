9421abcad7c02009d8867dad624094cf238eda4c
hgicquel <hgicquel@student.42.fr>
2022-04-06 16:42:26 +0200

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { NoSSR } from 'comps/next/nossr'
-import { ThemeProvider } from 'comps/theme/context'
+import { ThemeProvider, useTheme } from 'comps/theme/context'
+import { ChildrenProps } from 'libs/react/props'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function App(props: AppProps) {
@@ -8,7 +10,34 @@ export default function App(props: AppProps) {
 
   return <NoSSR>
     <ThemeProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   </NoSSR>
+}
+
+function Layout(props: ChildrenProps) {
+  const theme = useTheme()
+
+  useEffect(() => {
+    (window as any).theme = theme
+  }, [theme])
+
+  return <>
+    <div className="w-full max-w-[1200px] m-auto">
+      <div className="h-[100px]" />
+      <div className="w-full text-center font-pixel text-8xl underline">
+        pong.io
+      </div>
+      <div className="h-[100px]" />
+      <div className="flex items-center">
+        <button className="border-8 border-opposite p-4 font-bold font-pixel hover:scale-95 transition-transform">
+          Leaderboard
+        </button>
+      </div>
+      <div className="my-2" />
+      {props.children}
+    </div>
+  </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
 import { useTheme } from "comps/theme/context"
-import { useEffect, useLayoutEffect, useMemo, useState } from "react"
-
-const size = 100
-const width = size * 16
-const height = size * 9
+import { useLayoutEffect, useMemo, useState } from "react"
 
 export default function Page() {
   const theme = useTheme()
-
-  useEffect(() => {
-    console.log("theme", theme)
-  }, [theme])
 
   const [canvas, setCanvas] =
     useState<HTMLCanvasElement | null>(null)
@@ -37,20 +29,7 @@ export default function Page() {
   }, [context, theme])
 
   return <>
-    <div className="w-full max-w-[1200px] m-auto">
-      <div className="h-[100px]" />
-      <div className="w-full text-center font-pixel text-8xl underline">
-        pong.io
-      </div>
-      <div className="h-[100px]" />
-      <div className="flex items-center">
-        <button className="border-8 border-opposite p-4 font-bold font-pixel hover:scale-95 transition-transform">
-          Leaderboard
-        </button>
-      </div>
-      <div className="my-2" />
-      <canvas className="w-full aspect-video border-8 border-opposite text-red-500"
-        ref={setCanvas} />
-    </div>
+    <canvas className="w-full aspect-video border-8 border-opposite text-red-500"
+      ref={setCanvas} />
   </>
 }
