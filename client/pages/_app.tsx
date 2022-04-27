5d45f8115d10eaeb491958daff63463b124c7883
Piamias <victorpiamiaspro@outlook.fr>
2022-04-27 18:41:27 +0200

    new faa

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -75,13 +75,5 @@ export function LandingPage() {
 				CONNECT WITH 42
 			</button>
 		</div>
-		<div className="flex justify-center mt-20 mb-4">
-			<p className="text-center text-2xl font-pixel">Enter your Authentification Code :</p>
-		</div>
-		<div className="flex justify-center mt-8 mb-4">
-			<input className="shadow appearance-none border rounded py-2 px-3 font-pixel"
-				type="text" placeholder="Authentication Code">
-			</input>
-		</div>
 	</Layout>
 }
