dced9ecbdafed39a7787bb260bfcada1a64cd854
Piamias <victorpiamiaspro@outlook.fr>
2022-05-03 12:36:11 +0200

    request finish

--- client/pages/friend.tsx
+++ client/pages/friend.tsx
@@ -1,13 +1,14 @@
 import { FriendList, FriendRequest } from "comps/friend/friend";
 import { Layout } from "comps/layout/layout";
 import { api, asJson } from "libs/fetch/fetch";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Page() {
 
 	const [friendList, setFriendList] = useState(false)
 	const [friendRequest, setFriendRequest] = useState(false)
 	const [friendName, setFriendName] = useState("")
+	const [requestSend, setRequestSend] = useState(false)
 
 	const toggleFriendList = useCallback(() => {
 		setFriendList(friendList => !friendList)
@@ -19,13 +20,22 @@ export default function Page() {
 
 	const AddFriend = (friendName: string) => {
 		fetch(api("/friends/add"), { method: "POST", ...asJson({ username: friendName }) })
-		window.location.reload()
+		setRequestSend(true)
 	}
+
+	useEffect(() => {
+		const Id = setInterval(() => setRequestSend(false), 2000);
+		return () => clearInterval(Id)
+	}, [requestSend])
 
 	return (
 		<>
 			<Layout>
-				<div className="h-[50px]" />
+				<div className="h-[40px]" />
+				{requestSend === true ? <p className="text-center font-pixel text-emerald-600 underline pb-4">
+					Request Send
+				</p> :
+					<div className="pb-10" />}
 				<div className='flex justify-center'>
 					<a className="bg-zinc-800 flex flex-col text-center h-20 w-72 pt-5 rounded-lg border-8 scale-90 border-zinc-200 border-double cursor-grab hover:scale-105 duration-300 transition-transform"
 						onClick={() => AddFriend(friendName)}>
